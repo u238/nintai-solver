3f531f49d2d3bf621df73ded19624dcb18175ed7
Thomas Forrer <thomas.forrer@wuerth-phoenix.com>
2022-01-14 22:24:39 +0100

    implement further steps

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,17 @@ mod nintai_game;
 mod nintai_play;
 
 fn main() {
-    let g = nintai_game::GameArea::new(nintai_game::Colors::Yellow);
-    println!("The GameArea is: {:?}", g);
+    let mut g = nintai_game::GameArea::new(nintai_game::Colors::Yellow);
+    let c = g.get_piece_candidates();
+    // println!("The GameArea is: {:?}", g);
+    println!("The candidates count is: {:?}", c.iter().count());
+    println!("The candidates is: {:?}", c);
+
+    // g.pieces[0].position = 1;
+    // g.next_piece_index = 2;
+
+    // println!("The 3%2 is: {:?}", (3 % 2));
+    // println!("The 0+2%3 is: {:?}", ((0 + 2) % 3));
+    // println!("The 1+2%3 is: {:?}", ((1 + 2) % 3));
+    // println!("The 2+2%3 is: {:?}", ((2 + 2) % 3));
 }
--- src/nintai_game.rs
+++ src/nintai_game.rs
@@ -1,14 +1,28 @@
-#[derive(Debug)]
+#[derive(Clone, Debug, Copy)]
 pub enum Colors {
     Blue,
     Green,
     Yellow
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Piece {
     numbers: [u8; 3],
     position: u8
+}
+
+impl Piece {
+    pub fn get_number_of_color(self, color: Colors) -> u8 {
+        self.numbers[((color as u8) % 3) as usize]
+    }
+
+    pub fn get_right_side_number_of_base_color(&self, color: &Colors) -> u8 {
+        self.numbers[((color.clone() as u8 + 2) % 3) as usize]
+    }
+
+    pub fn get_left_side_number_of_base_color(&self, color: &Colors) -> u8 {
+        self.numbers[((color.clone() as u8 + 1) % 3) as usize]
+    }
 }
 
 #[derive(Debug)]
@@ -18,6 +32,15 @@ pub struct GameArea {
     next_piece_index: u8
 }
 
+fn generate_below_of_next_piece_index_rec(prev_base: &u8, curr_base: &u8, index: &u8) -> u8 {
+    return if index <= curr_base {
+        let r = *index - (*curr_base - *prev_base) + 1;
+        r
+    } else {
+        generate_below_of_next_piece_index_rec(curr_base, &(curr_base + (curr_base - prev_base - 2)), index)
+    }
+}
+
 impl GameArea {
     pub fn new(orientation: Colors) -> Self {
         Self {
@@ -25,6 +48,20 @@ impl GameArea {
             pieces: Self::get_new_pieces(),
             next_piece_index: 1
         }
+    }
+
+    pub fn get_next_piece_index(&self) -> &u8 {
+        &self.next_piece_index
+    }
+
+
+
+    pub fn get_below_of_next_piece_index(&self) -> u8 {
+        generate_below_of_next_piece_index_rec(&(0 as u8), &(11 as u8), self.get_next_piece_index())
+    }
+
+    pub fn get_orientation(&self) -> &Colors {
+        &self.orientation
     }
 
     pub fn get_new_pieces() -> [Piece; 36] {
@@ -73,12 +110,78 @@ impl GameArea {
         ]
     }
 
-    pub fn get_piece_candidates() -> Vec<Piece> {
-        let mut candidates : Vec::new();
-        let position = 1;
-        let value = 1;
-        Self.pieces.iter().filter(|&p| p.numbers[position] == value).
-        candidates
+    pub fn set_next_piece(&mut self, new_piece: Piece) {
+        let npi = self.get_next_piece_index().clone();
+        for p in self.pieces.iter_mut() {
+            if *p == new_piece {
+                p.position = npi
+            }
+        }
+        self.next_piece_index += 1;
+    }
+
+    pub fn get_piece_candidates(&self) -> Vec<Piece> {
+        match self.next_piece_index {
+            1  => {
+                self.pieces.iter().cloned().collect::<Vec<Piece>>()
+            },
+            3 | 5 | 7 | 9 | 11 => {
+                self.get_piece_candidates_match_left_side()
+            },
+            12 | 21 | 28 | 33 | 36 => {
+                self.pieces.iter().cloned().collect::<Vec<Piece>>()
+            },
+            14 | 16 | 18 | 20 | 23 | 25 | 27 | 30 | 32 | 35 => {
+                self.pieces.iter().cloned().collect::<Vec<Piece>>()
+            },
+            _ => {
+                self.get_piece_candidates_match_left_side_upside_down()
+            }
+        }
+    }
+
+    fn get_previous_piece_number_right_side(&self) -> u8 {
+        self.pieces
+            .iter()
+            .filter(|p| p.position == self.get_next_piece_index()-1)
+            .cloned()
+            .next()
+            .unwrap()
+            .get_right_side_number_of_base_color(self.get_orientation())
+    }
+
+    fn get_previous_piece_number_left_side(&self) -> u8 {
+        self.pieces
+            .iter()
+            .filter(|p| p.position == self.get_next_piece_index()-1)
+            .cloned()
+            .next()
+            .unwrap()
+            .get_left_side_number_of_base_color(self.get_orientation())
+    }
+
+    pub fn get_piece_candidates_match_left_side_upside_down(&self) -> Vec<Piece> {
+        // let c = ;
+        self.pieces
+            .iter()
+            .filter(|p| //p.numbers[c as usize] == 0 &&
+                p.position == 0 &&
+                p.get_right_side_number_of_base_color(self.get_orientation()) == self.get_previous_piece_number_right_side()
+            )
+            .cloned()
+            .collect::<Vec<Piece>>()
+    }
+
+    pub fn get_piece_candidates_match_left_side(&self) -> Vec<Piece> {
+        // let c = ;
+        self.pieces
+            .iter()
+            .filter(|p| //p.numbers[c as usize] == 0 &&
+                p.position == 0 &&
+                p.get_right_side_number_of_base_color(self.get_orientation()) == self.get_previous_piece_number_left_side()
+            )
+            .cloned()
+            .collect::<Vec<Piece>>()
     }
 }
 
@@ -113,5 +216,78 @@ mod tests {
                 assert!(g.pieces.iter().filter(|&p| p.numbers[position] == number).count() == 6)
             }
         }
+    }
+
+    #[test]
+    fn get_candidates_in_position_two_and_three() {
+        let mut g = GameArea::new(Colors::Blue);
+        // test position 2
+        let mut p = g.pieces[0].clone();
+        g.set_next_piece(p);
+        let mut c = g.get_piece_candidates();
+        assert_eq!(c.iter().count(), 5);
+
+        // test position 3
+        p = c[0].clone();
+        g.set_next_piece(p);
+        c = g.get_piece_candidates();
+        assert_eq!(c.iter().count(), 6);
+    }
+
+    #[test]
+    fn get_candidates_in_position_twelve() {
+        let mut g = GameArea::new(Colors::Blue);
+        // [1,2,3]
+        g.pieces[1].position = 2;
+        g.next_piece_index = 12;
+
+
+
+    }
+
+    #[test]
+    fn get_below_of_next_piece_index_all_lines() {
+        let mut g = GameArea::new(Colors::Blue);
+
+        g.next_piece_index = 2;
+        assert_eq!(g.get_below_of_next_piece_index(), 0 as u8);
+
+        g.next_piece_index = 11;
+        assert_eq!(g.get_below_of_next_piece_index(), 0 as u8);
+
+        g.next_piece_index = 12;
+        assert_eq!(g.get_below_of_next_piece_index(), 2 as u8);
+
+        g.next_piece_index = 20;
+        assert_eq!(g.get_below_of_next_piece_index(), 10 as u8);
+
+        g.next_piece_index = 21;
+        assert_eq!(g.get_below_of_next_piece_index(), 13 as u8);
+
+        g.next_piece_index = 27;
+        assert_eq!(g.get_below_of_next_piece_index(), 19 as u8);
+
+        g.next_piece_index = 28;
+        assert_eq!(g.get_below_of_next_piece_index(), 22 as u8);
+
+        g.next_piece_index = 32;
+        assert_eq!(g.get_below_of_next_piece_index(), 26 as u8);
+
+        g.next_piece_index = 33;
+        assert_eq!(g.get_below_of_next_piece_index(), 29 as u8);
+
+        g.next_piece_index = 35;
+        assert_eq!(g.get_below_of_next_piece_index(), 31 as u8);
+
+        g.next_piece_index = 36;
+        assert_eq!(g.get_below_of_next_piece_index(), 34 as u8);
+    }
+
+    #[test]
+    fn piece_get_number_of_specific_color() {
+        let p = Piece{numbers: [0, 1, 2], position: 0};
+        assert!(p.get_right_side_number_of_base_color(&Colors::Blue) == 2);
+        assert!(p.get_right_side_number_of_base_color(&Colors::Green) == 0);
+        assert!(p.get_right_side_number_of_base_color(&Colors::Yellow) == 1);
     }
 }
